4b4b3e6c1a22b71f639371649fb6ffacb0f7aaa6
jcperuggia <jessica.peruggia@hotmail.com>
2019-10-17 09:57:51 -0300

    aprimorando o js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var botaoAdicionar = document.querySelector("#adicionar-usuario");
+botaoAdicionar.addEventListener("click", function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+    var usuario = obtemUsuarioDoFormulario(form);
+    var erros = validaUsuario(usuario);
+
+    if (erros.length > 0){
+        exibeMenssagemDeErro(erros);
+        return;
+    }
+    adicionarUsuarioNaTabela(usuario);
+    
+    form.reset();
+
+    var menssagensErro = document.querySelector("#menssagens-erro");
+    menssagensErro.innerHTML = "";
+
+});
+
 function obtemUsuarioDoFormulario(form){
     var usuario ={
         nome: form.nome.value,
@@ -5,7 +26,7 @@ function obtemUsuarioDoFormulario(form){
         cintura: form.cintura.value,
         quadril: form.quadril.value,
         busto: form.busto.value,
-        biceps: form.bicpes.value,
+        biceps: form.biceps.value,
         coxa: form.coxa.value
     }
 
@@ -15,4 +36,39 @@ function obtemUsuarioDoFormulario(form){
 function montarTr(usuario){
     var usuarioTr = document.createElement("tr");
     usuarioTr.classList.add("usuario");
+
+    usuarioTr.appendChild(montarTd(usuario.nome, "info-nome"));
+    usuarioTr.appendChild(montarTd(usuario.peso, "info-peso"));
+    usuarioTr.appendChild(montarTd(usuario.cintura, "info-cintura"));
+    usuarioTr.appendChild(montarTd(usuario.quadril, "info-quadril"));
+    usuarioTr.appendChild(montarTd(usuario.busto, "info-busto"));
+    usuarioTr.appendChild(montarTd(usuario.biceps, "info-biceps"));
+    usuarioTr.appendChild(montarTd(usuario.coxa, "info-coxa"));
+
+    return usuarioTr;
+}
+
+function montarTd(dado, classe){
+    var td = document.createElement("td");
+    td.classList.add(classe);
+    td.textContent = dado;
+
+    return td;
+}
+
+function adicionarUsuarioNaTabela(usuario){
+    var usuarioTr = montarTr(usuario);
+    var tabela = document.querySelector("#tabela-usuarios");
+    tabela.appendChild(usuarioTr);
+}
+
+function exibeMenssagemDeErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
